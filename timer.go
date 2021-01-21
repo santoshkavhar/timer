1528f2f55c58fcdfee1cfbe9287ed4573da5b486
Santosh Kavhar <santosh.kavhar@joshsoftware.com>
2021-01-21 09:27:47 +0530

    pausyTimer struct renamed. NewTimer implemented

--- timer.go
+++ timer.go
@@ -4,8 +4,8 @@ import (
 	"time"
 )
 
-// Timer implementation with pause
-type pausyTimerInterface interface {
+// PausyInterface is the interface with pause, resume and stop with Durations
+type PausyInterface interface {
 	Pause() (time.Duration, bool)  // Duration Elapsed
 	Resume() (time.Duration, bool) // Duration Remaining
 	Stop() (time.Duration, bool)   // Duration Elapsed
@@ -19,10 +19,18 @@ const (
 	stopped
 )
 
-type pausyTimer struct {
+type Timer struct {
 	timer           *time.Timer
 	startTime       time.Time
 	elapsedDuration time.Duration
 	totalDuration   time.Duration
 	state           timerState
+}
+
+func NewTimer(duration time.Duration) (timerWithPause Timer) {
+	newTimer := time.NewTimer(duration)
+	timerWithPause.startTime = time.Now()
+	timerWithPause.totalDuration = duration
+	timerWithPause.timer = newTimer
+	return
 }
